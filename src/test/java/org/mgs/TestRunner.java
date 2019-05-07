56dc0196462a80b675fc6bcc4feec1f61a2a181d
M. Strizhov <m.strizhov@pflb.com>
2019-05-07 05:10:08 +0400

    Initial commit

---
+++ src/test/java/org/mgs/TestRunner.java
@@ -0,0 +1,14 @@
+package org.mgs;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = { "pretty"},
+        features = { "src/test/resources/features" })
+public class TestRunner {
+
+
+}
+
---
+++ src/test/resources/features/test1.feature
@@ -0,0 +1,5 @@
+Feature: test1
+  Scenario: test1 scenario
+    Given I have hungry snake
+    When When I feed my snake
+    Then I receive snek snek
